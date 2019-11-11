266d9cddef73c50cb728c2bdd4923c586c5f3030
Jennifer Stephan <jennifer.stephan@edu.escpeurope.eu>
2019-11-11 16:33:11 +0100

    adding fuse and db data to search

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,9 @@
 import "bootstrap";
+import { hot } from "react-hot-loader/root";
+import Search from "./search";
 
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+export default hot(Search);
